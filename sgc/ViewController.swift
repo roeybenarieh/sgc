733d335f8d6251e70efa7040cfb50144736698e2
roey ben arieh <roey280404@gmail.com>
2022-08-07 11:07:14 +0300

    Initial Commit

---
+++ sgc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sgc
+//
+//  Created by roey ben arieh on 07/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
