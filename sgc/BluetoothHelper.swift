b3d4388adf34176c10d5d4b7d42e779921a7154e
roey ben arieh <roey280404@gmail.com>
2022-08-11 10:32:52 +0300

    bluetooth helper class handle the serial object and notify views when something happen, TabBar change color when a new bluetooth module is appearing and a connection with it has established, when disconnects the color become red.

--- sgc/BluetoothHelper.swift
+++ sgc/BluetoothHelper.swift
@@ -8,24 +8,92 @@
 import Foundation
 import CoreBluetooth
 
-/// Global helper
-let helper: BluetoothHelper! = BluetoothHelper()
-
-final class BluetoothHelper{
+var Bhelper: BluetoothHelper!
+class BluetoothHelper: BluetoothSerialDelegate{
     
-    /// hold the list of all of the found bluetooth modules
+    //MARK: variables
+    
+    /// list of all of the found bluetooth modules
     var peripherals: [(peripheral: CBPeripheral, RSSI: Float)] = []
     
     
-    /// Add new peripheral to the peripheral list
-    func addPeripheral(_ peripheral: CBPeripheral, RSSI: NSNumber?){ //MY ADDITION
+    //MARK: initializer
+    
+    init(){
+        // init serial, it takes a few seconde until the bluetooth is on and than the serialDidChangeState() func is called
+        serial = BluetoothSerial(delegate: self)
+    }
+    
+    
+    //MARK: BluetoothSerialDelegate
+    
+    ///called when new peripheral is found
+    func serialDidDiscoverPeripheral(_ peripheral: CBPeripheral, RSSI: NSNumber?) {
+        ///making sure this isn't duplicate
         for exisiting in peripherals {
             if exisiting.peripheral.identifier == peripheral.identifier { return }
         }
         
-        // add to the array, next sort & reload
+        // add to the array, next sort
         let theRSSI = RSSI?.floatValue ?? 0.0
         peripherals.append((peripheral: peripheral, RSSI: theRSSI))
         peripherals.sort { $0.RSSI < $1.RSSI }
+        
+        /// there is no connection to a bluetooth module
+        if serial.connectedPeripheral == nil{
+            // connect to the peripheral
+            let selectedPeripheral = peripherals[0].peripheral
+            serial.connectToPeripheral(selectedPeripheral)
+            // making sure it's connected in 10sec
+            Timer.scheduledTimer(timeInterval: 10, target: self, selector: #selector(self.connectTimeOut), userInfo: nil, repeats: false)
+            //notify views that a new connection established
+            notifyBluetoothConnectionChanged(connectedToBluetooth:true)
+        }
+    }
+    
+    /// called when CBCentralManager changes (e.g. when bluetooth is turned on/off)
+    func serialDidChangeState() {
+        if serial.centralManager.state == .poweredOn {
+            print("bluetooth is turned on")
+            // start scanning
+            serial.startScan()
+        }
+        else{ ///Bluetooth is off
+            print("bluetooth is turned off, there is a problem")
+        }
+    }
+    /// Called when a peripheral disconnected
+    func serialDidDisconnect(_ peripheral: CBPeripheral, error: NSError?) {
+        //remove the disconnected peripheral from the list
+        for index in 0...peripherals.count-1{
+            if peripherals[index].peripheral.isEqual(peripheral){
+                peripherals.remove(at: index)
+                break
+            }
+        }
+        notifyBluetoothConnectionChanged(connectedToBluetooth:false)
+        serial.startScan()
+        print("something disconnected")
+    }
+    
+    //MARK: functions
+    
+    ///Should be called 10s after we've begun connecting, it makes sure the connection is ok
+    @objc func connectTimeOut() {
+        
+        // don't if we've already connected
+        if let _ = serial.connectedPeripheral {
+            print("the connection is working good")
+            return
+        }
+        else{
+            notifyBluetoothConnectionChanged(connectedToBluetooth:false)
+            serial.startScan()
+            print("bluetooth got disconnected after 10 seconds")
+        }
+    }
+    /// formating a notification post about a change in bluetooth connection
+    func notifyBluetoothConnectionChanged(connectedToBluetooth:Bool){
+        NotificationCenter.default.post(name: NSNotification.Name.init(rawValue: "Bluetoothchange"), object: nil, userInfo: ["connectedToBluetooth" : connectedToBluetooth])
     }
 }
--- sgc/TabBarController.swift
+++ sgc/TabBarController.swift
@@ -8,12 +8,13 @@
 import UIKit
 import CoreBluetooth // used for CBPeripheral
 
-class TabBarControllerr: UITabBarController, BluetoothSerialDelegate {
+class TabBarController: UITabBarController {
     
     //MARK: IBInspectable
     
-    /// a value that can be edited in the storyboard
-    @IBInspectable var initialIndex: Int = 0 ///the first view that will be displayed
+    ///the first view that will be displayed
+    @IBInspectable var initialIndex: Int = 0
+    
     
     //MARK: UITabBarController
     
@@ -23,81 +24,29 @@ class TabBarControllerr: UITabBarController, BluetoothSerialDelegate {
         //setting the view that initialy displayed
         selectedIndex = initialIndex
         
-        // init serial, it takes a few seconde until the bluetooth is on and than the serialDidChangeState() func is called
-        serial = BluetoothSerial(delegate: self)
-    }
-    
-    
-    //MARK: BluetoothSerialDelegate
-    
-    func serialDidDiscoverPeripheral(_ peripheral: CBPeripheral, RSSI: NSNumber?) {
-        helper.addPeripheral(peripheral, RSSI: RSSI)
-        
-        print("connecting to the first peripheral found")
-        //TODO: finish this
-        //found a peripheral stop scanning
-        serial.stopScan()
-        // connect to the peripheral
-        let selectedPeripheral = helper.peripherals[0].peripheral
-        serial.connectToPeripheral(selectedPeripheral)
-        // making sure it is connected
-        Timer.scheduledTimer(timeInterval: 10, target: self, selector: #selector(self.connectTimeOut), userInfo: nil, repeats: false)
-        //making the color of the tabbar green
-        changeTabBarColor(color: UIColor.systemGreen)
-    }
-    
-    /// called when CBCentralManager changes (e.g. when bluetooth is turned on/off)
-    func serialDidChangeState() {
-        print("bluetooth is turned on/off")
-        if serial.centralManager.state == .poweredOn {
-            // start scanning
-            serial.startScan()
-        }
-        else{ ///Bluetooth is off
-            NotificationCenter.default.post(name: Notification.Name(rawValue: "reloadStartViewController"), object: self)
-        }
-    }
-    /// Called when a peripheral disconnected
-    func serialDidDisconnect(_ peripheral: CBPeripheral, error: NSError?) {
-        //TODO: finish this?
-        changeTabBarColor(color: UIColor.systemRed)
-        serial.startScan()
-        print("something disconnect")
+        //initializing the bluetooth helper class
+        Bhelper = BluetoothHelper()
+        //listen to when a bluetooth connection has changed
+        NotificationCenter.default.addObserver(self, selector: #selector(bluetoothDisconnected(notification:)), name: NSNotification.Name.init(rawValue: "Bluetoothchange"), object: nil)
     }
 
     
     //MARK: Functnions
     
-    /// Should be called 10s after we've begun connecting
-    @objc func connectTimeOut() {
-        
-        // don't if we've already connected
-        if let _ = serial.connectedPeripheral {
-            print("the connection is working good")
-            return
+    ///function that listen when a connection has changed and act accordingly
+    @objc func bluetoothDisconnected(notification:NSNotification){
+        if let info = notification.userInfo{
+            let connected: Bool = info["connectedToBluetooth"] as! Bool
+            if connected{
+                changeTabBarColor(color:UIColor.systemGreen)
+            }
+            else{
+                changeTabBarColor(color:UIColor.systemRed)
+            }
         }
-        print("bluetooth got disconnected after 10 seconds")
-        //TODO: finish this
-        /*
-        if let _ = peripherals[0] {
-            serial.disconnect()
-            peripherals = []
-        }
-         */
     }
     
     public func changeTabBarColor(color:UIColor){
         self.tabBar.backgroundColor = color
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
