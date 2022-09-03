0408c86764bb85a1455faa4e32d03982608c1f1d
roey ben arieh <roey280404@gmail.com>
2022-09-03 11:43:16 +0300

    better GUI

--- sgc/SettingsScreen/SettingsVC.swift
+++ sgc/SettingsScreen/SettingsVC.swift
@@ -26,6 +26,8 @@ class SettingsVC: UIViewController, WKNavigationDelegate {
     //MARK: IBAction button function
     //step two in dexcomp api
     @IBAction func openDexcomAuthenticationPage(){
+        changeButtonColor(color: UIColor.systemBlue)
+        changeButtonText(text: "Connect to Dexcom account")
         let urlstr: String = "\(baseAuthenticationUrl)login?client_id=\(clientId)&redirect_uri=\(redirectUrl)&response_type=code&scope=offline_access&state=\(mystate)"
         webview.load(URLRequest(url: URL(string: urlstr)!))
         webview.navigationDelegate = self //telling the web view that this obj handle its navigation
@@ -35,20 +37,27 @@ class SettingsVC: UIViewController, WKNavigationDelegate {
     //MARK: webview delegates
     //step three in dexcom api
     func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void){
-        decisionHandler(.allow)
         if let url = navigationAction.request.url?.absoluteString{
             if url.hasPrefix(redirectUrl)
             {
-                getCodeFromRedirect(redirectUrl: url)
-                changeButtonColor(color: UIColor.systemGreen)
-                changeButtonText(text: "Dexcom account connected")
+                getCodeAndTokenFromRedirect(redirectUrl: url)
+                Thread.sleep(forTimeInterval: 5)
+                if hastoken{
+                    changeButtonColor(color: UIColor.systemGreen)
+                    changeButtonText(text: "Dexcom account connected")
+                }
+                else{
+                    changeButtonColor(color: UIColor.systemRed)
+                    changeButtonText(text: "Error: try again")
+                }
             }
         }
+        decisionHandler(.allow)
     }
     
     //MARK: dexcom button properties
     public func changeButtonColor(color: UIColor){
-        self.dexcomButton.tintColor = color
+        self.dexcomButton.backgroundColor = color
     }
     public func changeButtonText(text: String){
         self.dexcomButton.titleLabel?.text = text
