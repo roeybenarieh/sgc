abc502469dc28fee71814f570e4282f165c2e018
roey ben arieh <roey280404@gmail.com>
2022-09-17 13:30:50 +0300

    when injecting the last injection hour is being shown in the home page

--- sgc/HomeScreen/HomeVC.swift
+++ sgc/HomeScreen/HomeVC.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class HomeVC: UIViewController {
-
+    
+    //MARK: IBOoutlets
+    @IBOutlet weak var lastInjectionTime: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(self, selector: #selector(injectionConfirmed(notification:)), name: NSNotification.Name.init(rawValue: "InjectionConfirmation"), object: nil)
     }
 
-
+    @objc func injectionConfirmed(notification:NSNotification){
+        //ignore the message, it has to be confirmation beacuse that's the only message type the arduino can send
+        
+        //formating the time:
+        
+        // 1. Choose a date
+        let today = Date()
+        // 2. Pick the date components
+        let hours   = (Calendar.current.component(.hour, from: today))
+        let minutes = (Calendar.current.component(.minute, from: today))
+        // 3. Show the time
+        lastInjectionTime.text = "last Injection: " + "\(hours):\(minutes)"
+    }
 }
 
