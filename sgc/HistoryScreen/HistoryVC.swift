a5550419e9b8870a5fd6f0d59aa722ec6b1a3aab
roey ben arieh <roey280404@gmail.com>
2022-10-05 12:08:37 +0300

    every time injection happened - its logged in the history screen

--- sgc/HistoryScreen/HistoryVC.swift
+++ sgc/HistoryScreen/HistoryVC.swift
@@ -7,23 +7,47 @@
 
 import UIKit
 
-class HistoryVC: UIViewController {
 
+class HistoryVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    //MARK: variables
+    var historyArr :[(date: Date, message: String)] = []
+    
+    //MARK: IBOutlet
+    @IBOutlet weak var history: UITableView!
+    
+    //MARK: functions
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        history.delegate = self
+        history.dataSource = self
+        
+        //adding listeners
+        NotificationCenter.default.addObserver(self, selector: #selector(injectionConfirmed(notification:)), name: NSNotification.Name.init(rawValue: "InjectionConfirmation"), object: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: listeners
+    @objc func injectionConfirmed(notification:NSNotification){
+        historyArr.insert((date: Date(), message: "Completed injection succesfuly"), at: 0)
+        while historyArr.count > 100 {
+            historyArr.removeLast()
+        }
+        history.reloadData()
     }
-    */
+    
+    //MARK: tableView functions
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return historyArr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let templateCell = tableView.dequeueReusableCell(withIdentifier: "injectionCell", for: indexPath)
+                //templateCell.textLabel?.textColor = UIColor.systemGreen
+        let record = historyArr[indexPath.row]
+        templateCell.textLabel?.text = DateFormatter().string(from: record.date) + ":" + record.message
+        
+        return templateCell
+    }
 
 }
