80a323bfb2b1dbc210da0159b19d648052daccb8
roey ben arieh <roey280404@gmail.com>
2022-10-17 12:19:47 +0300

    clear the old 'connection to dexcom API' in the settings view - the app uses pydexcom instead

--- sgc/SettingsScreen/SettingsVC.swift
+++ sgc/SettingsScreen/SettingsVC.swift
@@ -6,44 +6,11 @@
 //
 
 import UIKit
-import WebKit
 
-class SettingsVC: UIViewController, WKNavigationDelegate {
-    
-    //MARK: IBOutlet variables
-    @IBOutlet weak var webview: WKWebView!
-    
-    @IBOutlet public weak var dexcomButton: UIButton!
-    
-    //MARK: UIViewController
+class SettingsVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    
-    //MARK: IBAction button function
-    //step two in dexcomp api
-    @IBAction func openDexcomAuthenticationPage(){
-        changeButtonColor(color: UIColor.systemBlue)
-        changeButtonText(text: "Connect to Dexcom account")
-        let urlstr: String = "https://google.com/"
-        webview.load(URLRequest(url: URL(string: urlstr)!))
-        webview.navigationDelegate = self //telling the web view that this obj handle its navigation
-    }
-    
-    
-    //MARK: webview delegates
-    //step three in dexcom api
-    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void){
-        decisionHandler(.allow)
-    }
-    
-    //MARK: dexcom button properties
-    public func changeButtonColor(color: UIColor){
-        self.dexcomButton.backgroundColor = color
-    }
-    public func changeButtonText(text: String){
-        self.dexcomButton.titleLabel?.text = text
-    }
 }
